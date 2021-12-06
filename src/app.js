c201ab43a203f320392871916c4c4ddd6940eb93
blofland <84938746+blofland@users.noreply.github.com>
2021-12-05 22:53:45 -0700

    Added routes to app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const path = require("path")
-
+const apiRouter = require("./routes/api/router")
+const viewRouter = require("./routes/notes/router")
 
 const app = express()
 const index = path.resolve(__dirname, "../public/index.html")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"));
-
+app.use("/api/notes", apiRouter)
+app.use("/notes", viewRouter)
 app.use("/", (req, res) => {
   res.sendFile(index)
 })
